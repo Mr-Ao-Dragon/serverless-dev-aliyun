4192683d07bdc83b8bc500a74bead0cd13797761
敖律风 <x123456789fy@outlook.com>
2023-08-01 07:58:16 +0800

    加上注释

--- code/main.go
+++ code/main.go
@@ -1,5 +1,7 @@
+// 包 main 是应用程序的入口点。
 package main
 
+// 导入必要的库。
 import (
 	"context"
 	"net/http"
@@ -7,16 +9,25 @@ import (
 	"github.com/aliyun/fc-runtime-go-sdk/fc"
 )
 
+// 主函数启动 HTTP 服务器。
 func main() {
 	fc.StartHttp(HandleHttpRequest)
 }
 
+// HandleHttpRequest 是 HTTP 处理程序函数。
 func HandleHttpRequest(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
+	// 设置 HTTP 响应状态码为 200 OK。
 	w.WriteHeader(http.StatusOK)
+
+	// 设置 Content-Type 头部为 text/plain。
 	w.Header().Add("Content-Type", "text/plain")
+
+	// 写入响应主体。
 	_, err := w.Write([]byte("hello there\n"))
 	if err != nil {
 		return err
 	}
+
+	// 如果没有错误，则返回 nil。
 	return nil
 }
